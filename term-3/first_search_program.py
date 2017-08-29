1b6a8640c562dcd884be1002ee8d79b38860697a
Qiang Lu <buaaluqiang@hotmail.com>
2017-08-29 11:17:51 -0600

    add first_search_program

---
+++ term-3/first_search_program.py
@@ -0,0 +1,46 @@
+# ----------
+# User Instructions:
+#
+# Define a function, search() that returns a list
+# in the form of [optimal path length, row, col]. For
+# the grid shown below, your function should output
+# [11, 4, 5].
+#
+# If there is no valid path from the start point
+# to the goal, your function should return the string
+# 'fail'
+# ----------
+
+# Grid format:
+#   0 = Navigable space
+#   1 = Occupied space
+
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0]]
+init = [0, 0]
+goal = [len(grid)-1, len(grid[0])-1]
+cost = 1
+
+delta = [[-1, 0], # go up
+         [ 0,-1], # go left
+         [ 1, 0], # go down
+         [ 0, 1]] # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+def search(grid,init,goal,cost):
+    # ----------------------------------------
+    # insert code here
+    # ----------------------------------------
+    current = init
+    cost = 0
+    visited = []
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            for i in range(len(delta)):
+                to_be_checked = current + delta[i]
+                value = grid[to_be_checked]
+    return path
